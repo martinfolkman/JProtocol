a3c60899537bd052926e06fdf3c960661bdcdfe7
Anders Eliasson <eliassonaand@yahoo.com>
2011-05-31 10:07:11 +0200

    changed handlers

--- src/org/jprotocol/framework/handler/HandlerContext.java
+++ src/org/jprotocol/framework/handler/HandlerContext.java
@@ -1,5 +1,7 @@
 package org.jprotocol.framework.handler;
 
+import static org.jprotocol.util.Contract.check;
+
 import org.jprotocol.framework.dsl.IProtocolLayoutFactory;
 import org.jprotocol.framework.handler.Handler.Type;
 
@@ -8,9 +10,36 @@ public class HandlerContext {
 	public final IProtocolState protocolState;
 	public final IProtocolSniffer sniffer;
 	public final IProtocolLayoutFactory factory;
-	public HandlerContext(IProtocolLayoutFactory factory, Type type, IProtocolState protocolState, IProtocolSniffer sniffer) {
+	public final boolean msbFirst;
+	public final String upperHeaderReceiveFieldName;
+	public final String upperHeaderSendFieldName;
+	public final int headerReceiveValue;
+	public final int headerSendValue;
+	public HandlerContext(
+			IProtocolLayoutFactory factory, 
+            Type type, 
+            boolean msbFirst, 
+            String upperHeaderRequestFieldName, 
+            String upperHeaderResponseFieldName,
+            int lowerHeaderRequestValue, 
+            int lowerHeaderResponseValue, 
+            IProtocolState protocolState,
+            IProtocolSniffer sniffer) {
 		this.factory = factory;
 		this.type = type;
+		this.msbFirst = msbFirst;
+        if (type == Type.Server) {
+            this.upperHeaderReceiveFieldName = upperHeaderRequestFieldName;
+            this.upperHeaderSendFieldName = upperHeaderResponseFieldName;
+            this.headerReceiveValue = lowerHeaderRequestValue;
+            this.headerSendValue = lowerHeaderResponseValue;
+        } else {
+        	check(type == Type.Client);
+            this.upperHeaderReceiveFieldName = upperHeaderResponseFieldName;
+            this.upperHeaderSendFieldName = upperHeaderRequestFieldName;
+            this.headerReceiveValue = lowerHeaderResponseValue;
+            this.headerSendValue = lowerHeaderRequestValue;
+        }
 		this.protocolState = protocolState;
 		this.sniffer = sniffer;
 	}
